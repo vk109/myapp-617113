ed02562129755b64381aa5c45423375871089dd7
vk109 <developer.vishal109@gmail.com>
2024-04-04 10:19:00 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use official Node.js image as base
+FROM --platform=linux/amd64 node:12
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy all other application files to the working directory
+COPY . .
+
+# Expose port 8000 to the outside world
+EXPOSE 8000
+
+# Command to run the application
+CMD ["node", "app.js"]
---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+app.get("/", function (req, res) {
+	res.send("Welcome to 2022!");
+});
+app.get("/user-agent", (req, res) => {
+	res.send("User Agent: " + req.headers["user-agent"] + "\n");
+});
+app.get("/health", (req, res) => {
+	res.status(200).json({ message: "Everything is All right" });
+});
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
