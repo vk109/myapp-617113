74a1a1af4cd59e4beb044692a2b68cd47ded4770
vk109 <developer.vishal109@gmail.com>
2024-04-12 11:42:32 +0530

    Updated the locals variable according to new aws server

--- locals.tf
+++ locals.tf
@@ -1,18 +1,18 @@
 locals {
-  bucket_name = "vk-tf-demo"
-  table_name  = "vkTfDemo"
+  bucket_name = "mm-tf-demo"
+  table_name  = "mmTfDemo"
 
-  ecr_repo_name = "demo-app-ecr-repo"
+  ecr_repo_name = "mm-app-ecr-repo"
 
-  demo_app_cluster_name        = "demo-app-cluster"
+  demo_app_cluster_name        = "mm-app-cluster"
   availability_zones           = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  demo_app_task_famliy         = "demo-app-task"
+  demo_app_task_famliy         = "mm-app-task"
   container_port               = 8000
-  demo_app_task_name           = "demo-app-task"
-  ecs_task_execution_role_name = "demo-app-task-execution-role"
+  demo_app_task_name           = "mm-app-task"
+  ecs_task_execution_role_name = "mm-app-task-execution-role"
 
-  application_load_balancer_name = "cc-demo-app-alb"
-  target_group_name              = "cc-demo-alb-tg"
+  application_load_balancer_name = "mm-demo-app-alb"
+  target_group_name              = "mm-demo-alb-tg"
 
-  demo_app_service_name = "cc-demo-app-service"
+  demo_app_service_name = "mm-demo-app-service"
 }
